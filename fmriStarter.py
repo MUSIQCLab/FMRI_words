64eadaf676ecb31bb7cf31f627efb83f03d63be3
asterakin <styl.stella@Gmail.com>
2015-11-11 12:54:04 -0800

    fixed sparse matrix problem

--- fmriStarter.py
+++ fmriStarter.py
@@ -1,20 +1,28 @@
 __author__ = 'Stella'
 
 import scipy.io as io
-import LassoClass
+#import LassoClass as lassoSolver
 from pylab import *
+from scipy import sparse
 
 
-def loadFmriData ():
-    fmri_train = io.mmread("subject1_fmri_std.train.mtx").tocsc() # sparse format
-    fmri_test = io.mmread("subject1_fmri_std.test.mtx").tocsc() # sparse format
-    wordid_train = io.mmread("subject1_wordid.train.mtx")
-    wordid_test = io.mmread("subject1_wordid.test.mtx")
-    wordfeature_centered = io.mmread("word_feature_centered.mtx")
-    wordfeature_std = io.mmread("word_feature_std.mtx")
-
+def main():
     dictionary = open("meta/dictionary.txt").read().splitlines()
     semantic_feature = open("meta/semantic_feature.txt").read().splitlines()
+    fmri_train = io.mmread("subject1_fmri_std.train.mtx")
+    fmri_train_sparse = sparse.csc_matrix (fmri_train)# sparse format
+
+    fmri_test = io.mmread("subject1_fmri_std.test.mtx") # sparse format
+
+    fmri_test_sparse = sparse.csc_matrix (fmri_test)# sparse format
+
+    wordid_train = io.mmread("subject1_wordid.train.mtx")
+    wordid_test = io.mmread("subject1_wordid.test.mtx")
+    wordfeature_std = io.mmread("word_feature_std.mtx")
+    wordfeature_centered = io.mmread("word_feature_centered.mtx")
 
 
-loadFmriData()
+
+
+if __name__ == '__main__':
+    main()
